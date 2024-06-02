b06fc3709db886525188c7111eecf2a90ef31065
Sourabh Jaiswal <sourabhrj31@gmail.com>
2024-06-02 21:49:07 +0800

    :sparkles: cardinality estimation

--- src/cardinality/README.md
+++ src/cardinality/README.md
@@ -18,6 +18,11 @@ You have a very large unsorted dataset with many duplicates, how do you estimate
 
    Simple solution though can be very inaccurate.
 
+2. Probabilistic Counting
+   From the [paper](https://www.cse.unsw.edu.au/~cs9314/07s1/lectures/Lin_CS9314_References/fm85.pdf) we can use the idea of
+   having consecutive leading/trailing zeroes to estimate the number of unique elements in the dataset. The probability of having
+   `k` zeroes would be `1/(2^(k+1))`. Performing multiple iterations of this can give us an estimate of the number of unique
+
 ## References
 
 - [Morris Algorithm](https://gregorygundersen.com/blog/2019/11/11/morris-algorithm/)
--- src/cardinality/mod.rs
+++ src/cardinality/mod.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 use rand::Rng;
 
 /// Counting the number of elements in a collection with less than O(logn) space
@@ -17,4 +19,28 @@ pub fn morris_counting(iter: impl Iterator) -> i32 {
     }
 
     x_n
+}
+
+pub fn estimate_cardinality(iter: impl Iterator<Item = u64>, k: u32) -> f32 {
+    let bucket_size: u32 = 1 << k;
+    let mut bucket = vec![0; bucket_size as usize];
+
+    for i in iter {
+        let h = hash(i);
+        let bucket_id = h & ((bucket_size - 1) as u64);
+        let bucket_hash = h >> k;
+        bucket[bucket_id as usize] =
+            max(bucket[bucket_id as usize], trailing_zeroes(bucket_hash));
+    }
+
+    let sum = bucket.iter().fold(0, |acc, x| acc + x);
+    (1 << (sum / bucket_size)) as f32 * bucket_size as f32 * 0.79402
+}
+
+fn trailing_zeroes(n: u64) -> u32 {
+    n.trailing_zeros()
+}
+
+fn hash(val: u64) -> u64 {
+    val
 }
